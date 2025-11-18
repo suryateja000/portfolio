daf52674764341bd6937344f042d10bb8f2212f0
suryateja000 <suryatejamedisetty000@gmail.com>
2025-11-18 22:52:34 +0530

    .

--- BacKend/llm.py
+++ BacKend/llm.py
@@ -1,12 +1,17 @@
 import os
-from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_llm(model: str="gemini-2.5-flash", temperature: float=0.3):
+def get_llm(model="gemini-1.5-flash", temperature=0.3):
+    api_key = os.getenv("GOOGLE_API_KEY")
+    
+    if not api_key:
+        raise ValueError("GOOGLE_API_KEY not found in environment variables")
+    
     return ChatGoogleGenerativeAI(
         model=model,
         temperature=temperature,
-        google_api_key=os.getenv("GOOGLE_API_KEY")
+        google_api_key=api_key
     )
